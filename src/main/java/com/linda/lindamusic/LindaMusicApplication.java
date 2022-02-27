4738aa983e18d7b379f694b0e4beea9ac7094566
林思涵 <linsh6@asiainfo.com>
2022-02-27 22:36:29 +0800

    Initial commit

---
+++ src/main/java/com/linda/lindamusic/LindaMusicApplication.java
@@ -0,0 +1,13 @@
+package com.linda.lindamusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindaMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LindaMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linda/lindamusic/LindaMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linda.lindamusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindaMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
